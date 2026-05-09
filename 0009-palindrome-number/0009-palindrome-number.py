25f55089c9f87dfda29364c9c242c86bbd31cd38
Eunhee <71442345+EunheeJeong1892@users.noreply.github.com>
2026-05-09 16:59:15 +0100

    .py 0009-palindrome-number Easy 05-09-2026 05-09-2026 0 ms (100%)

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,4 +1,10 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
-        
+        if x<0:
+            return False
+        else:
+            x = str(x)
+            if x == x[::-1]:
+                return True
+            else:
+                return False
